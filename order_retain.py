7c77521e59173de7b2d5eb2779f8672ae8a8915a
cYi1994 <yichuan941003@hotmail.com>
2017-08-10 11:48:38 +0800

    Update order_retain.py

--- order_retain.py
+++ order_retain.py
@@ -4,11 +4,11 @@
 import MySQLdb
 import csv
 
-db = MySQLdb.connect(host='10.44.62.51',
-                     port=3308,
-                     user='youzhangwei',
-                     passwd='Aftyouzw4rjm',
-                     db='afanti_online')
+db = MySQLdb.connect(host='',
+                     port=,
+                     user='',
+                     passwd='',
+                     db='')
 cur = db.cursor()
 
 sql = 'SELECT DISTINCT pid, \
